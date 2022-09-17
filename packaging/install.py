146cf0e9043bfad4fe626b67d9025d84f18395a6
Luigi Genovese <luigi.genovese@cea.fr>
2022-09-17 16:00:48 +0200

    installation procedure improved

--- packaging/install.py
+++ packaging/install.py
@@ -2,12 +2,13 @@ from os.path import join
 from os import sep
 school_url = 'https://gitlab.com/luigigenovese/bigdft-school'
 trunk_url =  '-b 1.9.3-new https://gitlab.com/luigigenovese/bigdft-suite.git'
-drive_path = join(sep,'content', 'drive')
-training_path = join(drive_path, 'MyDrive','bigdft-school')
-packaging_path = join(training_path, 'bigdft-school', 'packaging')
+base_path = join(sep,'content')
+drive_path = join(base_path, 'drive')
+training_path = join('bigdft_school')
+packaging_path = join('packaging')
 conda_package = join(packaging_path, 'bigdft-suite-1.9.3_rc.2-mpi_openmpi_py37h008c211_0.tar.bz2')
 extra_package = join(packaging_path, 'extra_libs.tar.bz2')
-prefix = join(training_path, 'install')
+prefix = join('install')
 bigdft_root_path = join(prefix, 'bin')
 bigdft_executable = join(bigdft_root_path, 'bigdft')
 bigdft_pythonpath = join(prefix, 'lib', 'python3.7', 'site-packages') # can use python-config for generality
@@ -25,19 +26,20 @@ def mount_drive():
   from google.colab import drive
   drive.mount(drive_path, force_remount=True)
 
-def change_dir():
-  from os import chdir
-  execute('mkdir','-p',training_path)
-  chdir(training_path)
+def change_dir(pwd=training_path):
+  from os import chdir, path
+  execute('mkdir','-p',path.abspath(pwd))
+  chdir(pwd)
 
 def get_repo(url=school_url):
   execute("git","clone",'--depth','1',url)
 
 def set_environment():
   from os import environ
+  from os.path import abspath
   from sys import path
-  environ['BIGDFT_ROOT'] = bigdft_root_path
-  path.insert(0, bigdft_pythonpath)
+  environ['BIGDFT_ROOT'] = abspath(bigdft_root_path)
+  path.insert(0, abspath(bigdft_pythonpath))
 
 def skip():
    from os.path import isfile
@@ -51,18 +53,52 @@ def untar_bz2(archive,dest='install'):
     execute('mkdir','-p',dest)
     execute('tar','xjf',archive,'-C',dest)
 
+def install_colab():
+    packages(path=None,options='-q','condacolab')
+
+def setup_colab():
+    import condacolab
+    condacolab.install()
 
 def install_bigdft():
-    get_repo()
-    untar_bz2(conda_package)
-    untar_bz2(extra_package)
+    from os import system
+    execute("conda","install",conda_package)
+    execute("conda","update","--all")
 
-def install_client():
+def get_conda():
+  install_colab()
+  setup_colab()
+
+def full_suite():
+    from os import environ
+    get_conda()
+    change_dir(base_path)
+    get_school_repo()
+    install_bigdft()
+    environ['BIGDFT_ROOT'] = ''
+
+def client(locally=False):
+    from os.path import join
+    if locally:
+       base = base_path
+    else:
+       base = join(drive_path,'MyDrive')
+       mount_drive()
+    change_dir(base)
+    get_school_repo()
     get_repo(url=trunk_url)
     execute('bigdft-suite/Installer.py','-f','ubuntu_MPI.rc','-a','no_upstream','build','bigdft-client','-y')
+    set_environment()
 
-def install_packages(*args):
-  execute('pip','install','-t',bigdft_pythonpath,args)
+def get_school_repo():
+    get_repo()
+    change_dir(training_path)
+
+def packages(path=bigdft_pythonpath,options='',*args):
+    if path is not None:
+      execute('pip','install','-t',path,options,*args)
+    else:
+      execute('pip','install',options,*args)
 
 def full_procedure():
   mount_drive()
@@ -71,4 +107,3 @@ def full_procedure():
     install_client()
   set_environment()
 
-full_procedure()
