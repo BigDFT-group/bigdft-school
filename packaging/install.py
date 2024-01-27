569de7cbe6e3db3f0cebd8999276efddfd94c1f6
Luigi Genovese <luigi.genovese@cea.fr>
2024-01-27 18:46:19 +0100

    install.py modified

--- packaging/install.py
+++ packaging/install.py
@@ -2,7 +2,7 @@ from os.path import join
 from os import sep
 school_url = 'https://github.com/BigDFT-group/bigdft-school'
 trunk_url = 'https://gitlab.com/l_sim/bigdft-suite.git'
-data_url = 'https://gitlab.com/luigigenovese/bigdft-school/-/raw/main/'
+data_url = 'https://raw.githubusercontent.com/BigDFT-group/bigdft-school/data'
 school_branch = 'devel'
 base_path = join(sep, 'content')
 drive_path = join(base_path, 'drive')
@@ -16,7 +16,7 @@ prefix = join('install')
 bigdft_root_path = join(prefix, 'bin')
 bigdft_executable = join(bigdft_root_path, 'bigdft')
 # can use python-config for generality
-bigdft_pythonpath = join(prefix, 'lib', 'python3.7', 'site-packages')
+bigdft_pythonpath = join(prefix, 'lib', 'python3.10', 'site-packages')
 
 
 def execute(*args):
@@ -54,12 +54,14 @@ def get_repo(url=school_url, options=[]):
     execute("git", "clone", '--depth', '1', *options, url)
 
 
-def set_environment():
+def set_environment(ppath):
     from os import environ, pathsep
     from os.path import abspath
     from sys import path
     environ['BIGDFT_ROOT'] = abspath(bigdft_root_path)
-    ppath = abspath(bigdft_pythonpath)
+    #ppath = abspath(bigdft_pythonpath)
+    if ppath is None:
+        return
     if ppath not in path:
         path.insert(0, ppath)
     environ['PYTHONPATH'] = pathsep.join(path)
@@ -70,9 +72,10 @@ def skip():
     return isfile(bigdft_executable)
 
 
-def ok_for_client():
+def ok_for_client(pip_path):
     from os.path import isdir
-    return isdir(bigdft_pythonpath)
+    if pip_path is not None:
+        return isdir(bigdft_pythonpath)
 
 
 def untar_archive(archive, dest='install'):
@@ -122,20 +125,26 @@ def set_ready():
 def client(locally=False):
     from os.path import join
     from os import environ
+    from sys import path
     if locally:
         base = base_path
+        pip_path=None
     else:
         base = join(drive_path, 'MyDrive')
         mount_drive()
+        pip_path=join(base,bigdft_pythonpath)
+        path.insert(0,pip_path)
     change_dir(base)
     get_school_repo()
-    if not ok_for_client():
-        get_repo(url=trunk_url, options=['-b', school_branch])
-        environ['JHBUILD_RUN_AS_ROOT'] = 'please do it'
-        execute('python', 'bigdft-suite/Installer.py', '-f',
-                'ubuntu_MPI.rc', '-a', 'no_upstream',
-                'build', 'bigdft-client', '-q', '-y')
-    set_environment()
+    if not ok_for_client(pip_path):
+        packages('pybigdft py3dmol ase spglib',
+                 path=pip_path, options=['-q'])
+        #get_repo(url=trunk_url, options=['-b', school_branch])
+        #environ['JHBUILD_RUN_AS_ROOT'] = 'please do it'
+        #execute('python', 'bigdft-suite/Installer.py', '-f',
+        #        'ubuntu_MPI.rc', '-a', 'no_upstream',
+        #        'build', 'bigdft-client', '-q', '-y')
+    set_environment(pip_path)
 
 
 def get_school_repo():
@@ -144,7 +153,8 @@ def get_school_repo():
 
 
 def get_remote_file(path, output):
-    execute('wget', data_url + path, '-O', output, '2>/dev/null')
+    from os.path import join
+    execute('wget', join(data_url, path), '-O', output)
 
 
 def packages(*args, path=bigdft_pythonpath, options=[]):
@@ -164,3 +174,35 @@ def purge_drive():
     from os.path import join
     change_dir(base_path)
     execute('rm', '-rf', join(drive_path, 'MyDrive', training_path))
+
+
+def get_sha_and_size(filename):
+    with open(filename) as ifile:
+        for line in ifile.readlines():
+            if 'sha256' in line:
+                sha=line.split(':')[-1].rstrip('\n')
+            if 'size' in line:
+                size=line.split()[-1].rstrip('\n')
+    return sha,size
+
+
+def get_curl_command(sha,size,repo):
+    basecurl=['curl', '-X', 'POST', '-H','"Accept: application/vnd.git-lfs+json"',
+              '-H','"Content-type: application/json"',
+              '-d']
+    datas='{"operation": "download", "transfer": ["basic"], "objects": [{"oid": "'+sha+'", "size": '+size+'}]}'
+    repos=repo+".git/info/lfs/objects/batch"
+    return basecurl+["'"+datas+"'",repos,'-o','href.json','--http1.1']
+
+
+def data_archive(archive, dest='.'):
+    from os import system, path
+    from json import load
+    lfs='lfs.info'
+    install.execute('wget', path.join(install.data_url,archive), '-O', lfs)
+    system(" ".join(get_curl_command(*get_sha_and_size(lfs),install.school_url)))
+    with open('href.json') as jfile:
+        href_d=load(jfile)
+    url=href_d['objects'][0]['actions']['download']['href']
+    install.execute('wget', url, '-O', path.basename(archive))
+    untar_archive(basename(archive), dest=dest)
